c0b5180e013f21327565cae90db8f5bc9e670b1c
Sarah Alli <desdequeensnyc@gmail.com>
2023-01-24 17:54:12 -0500

    getting started

---
+++ basics.py
@@ -0,0 +1,9 @@
+# Comments are declared with a hashtag.
+
+# Variables do not need to be declared in Python like they do in JavasScript.
+# Simply start with the name.
+
+my_name = "Sah"
+
+# Instead of console.log(), use print()
+print("Hello and welcome " + my_name + "!")
