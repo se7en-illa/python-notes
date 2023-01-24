a223cfc2e4882da7d96312892d1e6b66e40c0547
Sarah Alli <desdequeensnyc@gmail.com>
2023-01-24 18:04:27 -0500

    basics continued

--- basics.py
+++ basics.py
@@ -7,3 +7,28 @@ my_name = "Sah"
 
 # Instead of console.log(), use print()
 print("Hello and welcome " + my_name + "!")
+
+# Like JS, in Python strings can use either single or double quotes
+print("sah")
+print('sah')
+
+# Variables can be re-declared like JS
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = 'Turkey sandwich'
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Spaghetti with meatballs"
+# Printing out dinner
+print("Dinner:")
+print(meal)
