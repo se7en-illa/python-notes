cc08103d21277990481e220d0d9af165d6ee90f3
Sarah Alli <desdequeensnyc@gmail.com>
2023-01-24 20:34:43 -0500

    list methods

--- Lists/lists.py
+++ Lists/lists.py
@@ -108,3 +108,102 @@ incoming_class = [["Kenny", "American", 9], [
 incoming_class[2][2] = 8
 incoming_class[-3][-3] = "Ken"
 print(incoming_class)
+
+
+# Python List Methods
+# .count( - A list method to count the number of occurrences of an element in a list.
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie",
+         "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+jake_votes = votes.count("Jake")
+print(jake_votes)
+
+# .insert() - A list method to insert an element into a specific index of a list.
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+front_display_list.insert(0, "Pineapple")
+print(front_display_list)
+
+# •pop() - A list method to remove an element from a specific index or from the end of a list.
+data_science_topics = ["Machine Learning", "SQL",
+                       "Pandas", "Algorithms", "Statistics", "Python 3"]
+data_science_topics.pop()
+data_science_topics.pop(-2)
+print(data_science_topics)
+
+# range() - A built-in Python function to create a sequence of integers.
+number_list = range(0, 9)
+print(list(number_list))
+
+zero_to_seven = range(8)
+print(list(zero_to_seven))
+
+range_five_three = range(5, 15, 3)
+range_diff_five = range(0, 40, 5)
+
+# len() - A built-in Python function to get the length of a list.
+long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that",
+             "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+long_list_len = len(long_list)
+print(long_list_len)
+
+big_range = range(2, 3000, 100)
+big_range_length = len(big_range)
+print(big_range_length)
+
+# slice
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:2]
+print(beginning)
+
+middle = suitcase[2:4]
+print(middle)
+
+last_two_elements = suitcase[-2:]
+print(last_two_elements)
+
+slice_off_last_three = suitcase[:-3]
+print(slice_off_last_three)
+
+# sort@/ sortedO-A method and a built- in function to sort a list
+# Checkpoint 1 & 2
+addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place",
+             "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+
+print(addresses)
+addresses.sort()
+print(addresses)
+
+# Checkpoint 3
+names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
+names.sort()
+print(names)
+
+
+# Checkpoint 4 & 5
+cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
+cities.sort(reverse=True)
+print(cities)
+
+
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+games_sorted = sorted(games)
+
+print(games)
+print(games_sorted)
+
+
+inventory = ["twin bed", "twin bed", "headboard", "queen bed", "king bed", "dresser", "dresser", "table", "table",
+             "nightstand", "nightstand", "king bed", "king bed", "twin bed", "twin bed", "sheets", "sheets", "pillow", "pillow"]
+
+inventory_len = len(inventory)
+
+first = inventory[0]
+last = inventory[-1]
+inventory_2_6 = inventory[2:6]
+first_3 = inventory[0:3]
+twin_beds = inventory.count("twin bed")
+removed_item = inventory.pop(4)
+inventory.insert(10, "19th Century Bed Frame")
+inventory = sorted(inventory)
